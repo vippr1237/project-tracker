d6929d4d8a346393d4e63760221d59ba24d62a45
unknown <vippr1239@gmail.com>
2022-01-07 23:54:25 +0700

    Add docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM mongo:latest
 
 ENV MONGO_INITDB_ROOT_USERNAME admin
 ENV MONGO_INITDB_ROOT_PASSWORD admin123
-ENV MONGO_INITDB_DATABASE project-tracker
+
 
 ADD mongo-init.js /docker-entrypoint-initdb.d/
+
+EXPOSE 27017
--- mongo-init.js
+++ mongo-init.js
@@ -1,12 +1,13 @@
-db.createUser(
-    {
-        user: "admin",
-        pwd: "admin123",
-        roles: [
-            {
-                role: "readWrite",
-                db: "project-tracker"
-            }
-        ]
-    }
-);
+db = db.getSiblingDB('admin');
+// move to the admin db - always created in Mongo
+db.auth("admin", "admin123");
+// log as root admin if you decided to authenticate in your docker-compose file...
+db = db.getSiblingDB('project-tracker');
+// create and move to your new database
+db.createUser({
+'user': "dbUser",
+'pwd': "dbPwd",
+'roles': [{
+    'role': 'dbOwner',
+    'db': 'project-tracker'}]});
+// user created
